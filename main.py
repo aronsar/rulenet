d0a0306fe7ccfa972088606d2668926cacdd4f53
Aron <aronsar@gmail.com>
2018-07-12 12:11:46 -0700

    outlined the network

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import tensorflow as tf
 import numpy as np
 import pdb
 
-RANDOM_SEED = 42
-np.random.seed(RANDOM_SEED)
-tf.set_random_seed(RANDOM_SEED)
-
 DATADIR = './new_facebook/'
 USER_ID = '0'
+RANDOM_SEED = 42
+
+np.random.seed(RANDOM_SEED)
+tf.set_random_seed(RANDOM_SEED)
 
 class Alter:
   def __init__(self, feature_vec, mutual_friends_vec, circle_membership):
@@ -51,7 +51,7 @@ def dataReader(data_dir=DATADIR, user_id=USER_ID):
       for member in line.split(' ')[1:]: # we ignore the first word, which is just the circle index
         circle_membership_matrix[int(member),circle_index] = 1
   
-  # the following for loop iterates over row vectors of two matrices
+  # the following for loop creates an array of alter objects
   for mutual_friend in range(num_mutual_friends):
     alter_array.append(Alter(feature_matrix[mutual_friend, :],
                              mutual_friends_matrix[mutual_friend, :],
@@ -64,10 +64,34 @@ def dataReader(data_dir=DATADIR, user_id=USER_ID):
   
   return train_alters, test_alters
 
-
-
 def main():
   train_alters, test_alters = dataReader(DATADIR, USER_ID)
-
+  num_alters = len(train_alters)
+  
+  # Training
+  graph = tf.Graph()
+  
+  with graph.as_default():
+    # get an alter
+    step_ph = tf.placeholder(dtype=tf.float32, shape=())
+    alter = train_alters[step_ph % num_alters]
+    
+    # alter.mutual_friends_vec is input [1 x num_alters]
+    # multiply by weight matrix [num_alters x h1]
+    # add in bias [1 x h1]
+    # activation function (ReLu)
+    
+    # multiply input by weight matrix [1 x h1] * [h1 x num_feat]
+    # do one of:
+      # add in logical feature vector as bias
+      # multiply in the logical feature vector as bias
+      # transform logical feature vector from [0,1] to [1,10] and multiply in as bias
+    
+    # do one more fc layer, where the size of the logits is [1 x num_circles]
+    
+    # multi-class, multi-label loss
+    loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=alter.circle_membership,
+                                                 logits=
+  
 if __name__ == '__main__':
     main()
